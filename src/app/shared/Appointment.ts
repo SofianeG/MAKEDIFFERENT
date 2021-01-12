f24f3e2ded29ff95e15ea1fc0c151c6d578a3cd3
SofianeG <sofianegougam17@gmail.com>
2021-01-12 16:39:08 +0100

    MakeDifferent App

---
+++ src/app/shared/Appointment.ts
@@ -0,0 +1,12 @@
+import { Timestamp } from 'rxjs';
+
+export class Appointment {
+    $key: string;
+    id:string;
+    name: string;
+    email: string
+    mobile: number;
+
+    // start:Date;
+   
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD_iMJQ1SbSV_QnZ0dOiBkcOHFm1prG6Xo",
+    authDomain: "make-different-10e2f.firebaseapp.com",
+    projectId: "make-different-10e2f",
+    storageBucket: "make-different-10e2f.appspot.com",
+    messagingSenderId: "1082987763705",
+    appId: "1:1082987763705:web:d1646e4b81a8ffffbc8f28"
+  }
+};
